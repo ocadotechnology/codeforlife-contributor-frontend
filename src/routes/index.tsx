b53fa19f6e13f66631ee02fbc808e0687f860efe
Stefan Kairinos <skairinos@gmail.com>
2024-08-16 18:20:15 +0300

    Initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import fruit from "./fruit"
+import general from "./general"
+import paths from "./paths"
+
+const routes = (
+  <>
+    {general}
+    {fruit}
+  </>
+)
+
+export default routes
+export { paths }
