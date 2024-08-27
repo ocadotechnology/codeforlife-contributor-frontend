15168506b0bfd2b667c60ee9c30f2d1abadb27d5
SalmanAsh <salman.ashraf2513@gmail.com>
2024-08-27 12:49:08 +0000

    add login endpoint

---
+++ src/api/session.ts
@@ -0,0 +1,22 @@
+import { type SessionMetadata } from "../app/hooks"
+
+const baseUrl = "session/"
+export type LoginWithGithubResult = SessionMetadata
+export type LoginWithGithubArg = { code: string }
+
+import api from "."
+
+const sessionApi = api.injectEndpoints({
+  endpoints: build => ({
+    loginWithGithub: build.mutation<LoginWithGithubResult, LoginWithGithubArg>({
+      query: body => ({
+        url: baseUrl + "login-with-github/",
+        method: "POST",
+        body,
+      }),
+    }),
+  }),
+})
+
+export const { useLoginWithGithubMutation } = sessionApi
+export default sessionApi
