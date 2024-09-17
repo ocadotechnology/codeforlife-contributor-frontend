13957bb586fab0618bbab9525fe04f6e39343d1e
SKairinos <skairinos@gmail.com>
2024-09-17 09:40:02 +0000

    redirect to home page if not found

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
+import { Navigate, Route } from "react-router-dom"
+
 import agreementSignature from "./agreementSignature"
 import general from "./general"
+import paths from "./paths"
 
 const routes = (
   <>
     {general}
     {agreementSignature}
+    <Route path="*" element={<Navigate to={paths._} replace />} />
   </>
 )
 
