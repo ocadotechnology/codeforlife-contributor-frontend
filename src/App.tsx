7a99b553536e518f0f597d98da0762a8c2c555c2
SKairinos <stefan.kairinos@ocado.com>
2024-12-12 17:20:46 +0000

    feat: ssr

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+import { App as _App, type AppProps as _AppProps } from "codeforlife/components"
 import { type FC } from "react"
-import { App as _ } from "codeforlife/components"
 
 import { Header } from "./components"
 import routes from "./routes"
 import store from "./app/store"
 import theme from "./app/theme"
 
-export interface AppProps {}
+export interface AppProps extends Pick<_AppProps, "path"> {}
 
-const App: FC<AppProps> = () => (
-  <_ store={store} theme={theme} routes={routes} header={<Header />} />
+const App: FC<AppProps> = props => (
+  <_App
+    store={store}
+    theme={theme}
+    routes={routes}
+    header={<Header />}
+    {...props}
+  />
 )
 
 export default App
